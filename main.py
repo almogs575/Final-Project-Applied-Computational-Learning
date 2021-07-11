8f5ddcc68bbabaf489c92f008284c0a3d967556f
almogs575 <almogs575@gmail.com>
2021-07-11 16:14:15 +0300

    after merging friedman test

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import time
 import pandas as pd
 from pandas import read_csv
-from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.losses import categorical_crossentropy
@@ -16,7 +15,8 @@ optuna.logging.set_verbosity(optuna.logging.WARN)
 from statistics import mean
 from scipy import stats
 from tensorflow.keras.utils import to_categorical
-
+import tensorflow_datasets as tfds
+import cv2
 
 class lookAhead:
     name = ""
@@ -87,6 +87,46 @@ class lookAhead:
 
         return mean(acc_per_fold)
 
+    def load_data(self, dataset_name):
+
+        X_train, y_train = tfds.as_numpy(tfds.load(
+            dataset_name,
+            split='train',
+            batch_size=-1,
+            as_supervised=True,
+        ))
+
+        X_test, y_test = tfds.as_numpy(tfds.load(
+            dataset_name,
+            split='test',
+            batch_size=-1,
+            as_supervised=True,
+        ))
+
+        res_img = []
+        for img in X_train:
+            res = cv2.resize(img, (28, 28), interpolation=cv2.INTER_AREA)
+            res_img.append(res)
+        X_train = np.asarray(res_img)
+        res_img = []
+        for img in X_test:
+            res = cv2.resize(img, (28, 28), interpolation=cv2.INTER_AREA)
+            res_img.append(res)
+        X_test = np.asarray(res_img)
+
+        if (dataset_name == 'fashion_mnist' or dataset_name == 'kmnist' or dataset_name == 'mnist' or dataset_name == 'svhn_cropped' or dataset_name == 'cifar10'
+                or dataset_name == 'cifar100' or dataset_name == 'mnist_corrupted' or dataset_name == 'smallnorb'):
+
+            if dataset_name == 'fashion_mnist' or dataset_name == 'kmnist' or dataset_name == 'mnist' or dataset_name == 'mnist_corrupted' or dataset_name == 'smallnorb':
+                X_train = np.expand_dims(X_train, axis=-1)
+                X_test = np.expand_dims(X_test, axis=-1)
+
+            return X_train[:1000], X_test[:500], y_train[:1000], y_test[:500]
+
+        else:
+            return X_train, X_test, y_train, y_test
+
+
     def normalize_data(self, X_train, X_test):
         # Parse numbers as floats
         X_train = X_train.astype('float32')
@@ -100,16 +140,9 @@ class lookAhead:
 
     def model_training(self, dataset_name, algorithm_name):
 
-        # Load dataset
-        if dataset_name == 'cifar10':
-            (X_train, y_train), (X_test, y_test) = keras.datasets.cifar10.load_data()
-            self.shape = (X_train.shape[1], X_train.shape[2], X_train.shape[3])
 
-        if dataset_name == 'fashion_mnist':
-            (X_train, y_train), (X_test, y_test) = keras.datasets.fashion_mnist.load_data()
-            X_train = np.expand_dims(X_train, axis=-1)
-            X_test = np.expand_dims(X_test, axis=-1)
-            self.shape = (X_train.shape[1], X_train.shape[2], X_train.shape[3])
+        # Load dataset
+        X_train, X_test, y_train, y_test = self.load_data(dataset_name)
 
         # normalize
         X_train, X_test = self.normalize_data(X_train, X_test)
@@ -117,94 +150,88 @@ class lookAhead:
         # num of classes
         self.classes = np.unique(y_test).size
 
-        # splitting data to smaller datasets
-        X_train_split = np.array_split(X_train, 10)
-        y_train_split = np.array_split(y_train, 10)
-        X_test_split = np.array_split(X_test, 10)
-        y_test_split = np.array_split(y_test, 10)
-        datasets_amount = 10
+        # shape
+        self.shape = (X_train.shape[1], X_train.shape[2], X_train.shape[3])
 
-        for i in range(datasets_amount):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
 
-            self.X_train = X_train_split[i]
-            self.y_train = y_train_split[i]
-            self.X_test = X_test_split[i]
-            self.y_test = y_test_split[i]
+        # Define the K-fold Cross Validator
+        kfold = KFold(n_splits=10, shuffle=True)
 
-            # Define the K-fold Cross Validator
-            kfold = KFold(n_splits=10, shuffle=True)
+        # K-fold Cross Validation model evaluation
+        fold_no = 1
 
-            # K-fold Cross Validation model evaluation
-            fold_no = 1
+        for train, test in kfold.split(self.X_train, self.y_train):
 
-            for train, test in kfold.split(self.X_train, self.y_train):
+            self.data_array = []
+            self.data_array.append(dataset_name)
+            self.data_array.append(algorithm_name)
+            self.data_array.append(fold_no)
+            study = optuna.create_study(direction="maximize")
+            study.optimize(self.objective, n_trials=50)
+            self.data_array.append(study.best_params)
 
-                self.data_array = []
-                self.data_array.append(dataset_name + "_part_" + str(i))
-                self.data_array.append(algorithm_name)
-                self.data_array.append(fold_no)
-                study = optuna.create_study(direction="maximize")
-                study.optimize(self.objective, n_trials=50)
-                self.data_array.append(study.best_params)
+            # Generate a print
+            print('------------------------------------------------------------------------')
+            print(f'Training for fold {fold_no} ...')
 
-                # Generate a print
-                print('------------------------------------------------------------------------')
-                print(f'Training for fold {fold_no} ...')
+            model = self.getModel()
+            # Compile the model
+            if self.lookAhead_model:
+                model.compile(loss=categorical_crossentropy,
+                              optimizer=Lookahead(optimizer=study.best_params['optimizer'],
+                                                  slow_step_size=study.best_params['slow_step_size']),
+                              metrics=['accuracy', tf.keras.metrics.SensitivityAtSpecificity(0.5),
+                                       tf.keras.metrics.SpecificityAtSensitivity(0.5), tf.keras.metrics.Precision(),
+                                       tf.keras.metrics.AUC(), tf.keras.metrics.AUC(curve='PR')])
+            if self.improved_model:
+                model.compile(loss=categorical_crossentropy,
+                              optimizer=Lookahead(optimizer=Lookahead(optimizer='adam'),
+                                                  sync_period=study.best_params['sync_period'],
+                                                  slow_step_size=study.best_params['slow_step_size']),
+                              metrics=['accuracy', tf.keras.metrics.SensitivityAtSpecificity(0.5),
+                                       tf.keras.metrics.SpecificityAtSensitivity(0.5), tf.keras.metrics.Precision(),
+                                       tf.keras.metrics.AUC(), tf.keras.metrics.AUC(curve='PR')])
 
-                model = self.getModel()
-                # Compile the model
-                if self.lookAhead_model:
-                    model.compile(loss=categorical_crossentropy,
-                                  optimizer=Lookahead(optimizer=study.best_params['optimizer'],
-                                                      slow_step_size=study.best_params['slow_step_size']),
-                                  metrics=['accuracy', tf.keras.metrics.SensitivityAtSpecificity(0.5),
-                                           tf.keras.metrics.SpecificityAtSensitivity(0.5), tf.keras.metrics.Precision(),
-                                           tf.keras.metrics.AUC(), tf.keras.metrics.AUC(curve='PR')])
-                if self.improved_model:
-                    model.compile(loss=categorical_crossentropy,
-                                  optimizer=Lookahead(optimizer=Lookahead(optimizer='adam'),
-                                                      sync_period=study.best_params['sync_period'],
-                                                      slow_step_size=study.best_params['slow_step_size']),
-                                  metrics=['accuracy', tf.keras.metrics.SensitivityAtSpecificity(0.5),
-                                           tf.keras.metrics.SpecificityAtSensitivity(0.5), tf.keras.metrics.Precision(),
-                                           tf.keras.metrics.AUC(), tf.keras.metrics.AUC(curve='PR')])
+            if self.simple_model:
+                model.compile(loss=categorical_crossentropy,
+                              optimizer=Adam(learning_rate=study.best_params['lr'],
+                                             epsilon=study.best_params['epsilon']),
+                              metrics=['accuracy', tf.keras.metrics.SensitivityAtSpecificity(0.5),
+                                       tf.keras.metrics.SpecificityAtSensitivity(0.5), tf.keras.metrics.Precision(),
+                                       tf.keras.metrics.AUC(), tf.keras.metrics.AUC(curve='PR')])
 
-                if self.simple_model:
-                    model.compile(loss=categorical_crossentropy,
-                                  optimizer=Adam(learning_rate=study.best_params['lr'],
-                                                 epsilon=study.best_params['epsilon']),
-                                  metrics=['accuracy', tf.keras.metrics.SensitivityAtSpecificity(0.5),
-                                           tf.keras.metrics.SpecificityAtSensitivity(0.5), tf.keras.metrics.Precision(),
-                                           tf.keras.metrics.AUC(), tf.keras.metrics.AUC(curve='PR')])
+            fit_start = time.time()
+            # Fit data to model
+            model.fit(self.X_train[train], to_categorical(self.y_train[train]),
+                      validation_data=(self.X_train[test], to_categorical(self.y_train[test])), batch_size=64,
+                      epochs=10, verbose=1)
+            stop_fit = time.time() - fit_start
 
-                fit_start = time.time()
-                # Fit data to model
-                model.fit(self.X_train[train], to_categorical(self.y_train[train]),
-                          validation_data=(self.X_train[test], to_categorical(self.y_train[test])), batch_size=64,
-                          epochs=10, verbose=1)
-                stop_fit = time.time() - fit_start
+            # Generate generalization metrics
+            scores = model.evaluate(self.X_test, to_categorical(self.y_test), verbose=0)
 
-                # Generate generalization metrics
-                scores = model.evaluate(self.X_test, to_categorical(self.y_test), verbose=0)
+            inference_start = time.time()
+            model.predict(self.X_test)
+            inference_stop = time.time() - inference_start
 
-                inference_start = time.time()
-                model.predict(self.X_test)
-                inference_stop = time.time() - inference_start
+            self.data_array.append(scores[1] * 100)
+            self.data_array.append(scores[2])
+            self.data_array.append(scores[3])
+            self.data_array.append(scores[4])
+            self.data_array.append(scores[5])
+            self.data_array.append(scores[6])
+            self.data_array.append(stop_fit)
+            self.data_array.append(inference_stop)
 
-                self.data_array.append(scores[1] * 100)
-                self.data_array.append(scores[2])
-                self.data_array.append(scores[3])
-                self.data_array.append(scores[4])
-                self.data_array.append(scores[5])
-                self.data_array.append(scores[6])
-                self.data_array.append(stop_fit)
-                self.data_array.append(inference_stop)
-
-                self.data.append(self.data_array)
-                fold_no = fold_no + 1
+            self.data.append(self.data_array)
+            fold_no = fold_no + 1
 
     def friedman_test(self):
-        self.create_AUC_list("cname.csv")
+        self.create_AUC_list("results.csv")
         results = stats.friedmanchisquare(self.auc_adam, self.auc_lookAhead, self.auc_improved_lookAhead)
         print(results)
         if results[1] < 0.05:
@@ -230,7 +257,7 @@ class lookAhead:
 
 
 if __name__ == '__main__':
-    datasets = ['cifar10','fashion_mnist']  # 'emnist', 'fashion_mnist']  #,'cmaterdb' 'kmnist', 'mnist', 'mnist_corrupted','svhn_cropped']
+    datasets = ['cifar10','smallnorb','svhn_cropped','mnist_corrupted','mnist','kmnist','fashion_mnist','cifar100','cmaterdb','cmaterdb/devanagari','cmaterdb/telugu','beans','rock_paper_scissors','horses_or_humans']
     look = lookAhead()
     for x in range(3):
         if x == 0:
